fce50d5e25ea055c243bb78ecc50e556ae2f2187
DavidEsdrs <carpina1983@gmail.com>
2023-01-26 14:18:21 -0300

    Add 'getProducts' function and its implementation

--- src/repositories/IProductsRepository.ts
+++ src/repositories/IProductsRepository.ts
@@ -4,4 +4,5 @@ export interface IProductsRepository {
     findByTitle(title: string): Promise<Product>;
     create(args: Partial<Product>): Product;
     save(product: Product): Promise<void>;
+    getProducts(args?: { take?: number }): Promise<Product[]>
 }
--- src/repositories/implementations/typeorm/ProductsRepository.ts
+++ src/repositories/implementations/typeorm/ProductsRepository.ts
@@ -5,5 +5,10 @@ export const productsRepository = AppDataSource.getRepository(Product).extend({
     async findByTitle(title: string) {
         const product = await this.findOneBy({ title });
         return product;
+    },
+
+    async getProducts(args?: { take?: number; }) {
+        const products = await this.find({ take: args.take });
+        return products;
     }
 });
