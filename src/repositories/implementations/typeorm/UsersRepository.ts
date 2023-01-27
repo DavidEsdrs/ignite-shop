a0140e0fce2d01c682ae7ffd9deff791c5601afe
DavidEsdrs <carpina1983@gmail.com>
2023-01-27 12:20:34 -0300

    TODO: UsersRepository

--- src/repositories/implementations/typeorm/UsersRepository.ts
+++ src/repositories/implementations/typeorm/UsersRepository.ts
@@ -1,4 +1,4 @@
-import AppDataSource from "../../../../ormconfig";
+import AppDataSource from "../../../ormconfig";
 import { User } from "../../../entities/User";
 
 export const usersRepository = AppDataSource.getRepository(User).extend({
