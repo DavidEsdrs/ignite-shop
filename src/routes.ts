36ab5b0e122e501fa102f8d379675bf293798783
DavidEsdrs <carpina1983@gmail.com>
2023-01-27 17:26:40 -0300

    Add body validation middleware to the get product route

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,18 @@
 import { Router } from "express";
 import { buildAddProduct } from "./services/products/AddProduct/buildAddProductService";
 import { buildGetProduct } from "./services/products/GetProduct/buildGetProduct";
+import { validateSelectOptions } from "./services/products/GetProduct/GetProduct.middleware";
 import { buildGetProducts } from "./services/products/GetProducts/buildGetProducts";
 import { buildCreateUser } from "./services/users/CreateUser/buildCreateUser";
 
 const router = Router();
 
 // Products
-router.post("/product", (req, res) => buildAddProduct().handle(req, res));
+router.post("/products", (req, res) => buildAddProduct().handle(req, res));
 
 router.get("/products/all", (req, res) => buildGetProducts().handle(req, res));
 
-router.get("/product/:id", (req, res) => buildGetProduct().handle(req, res));
+router.get("/products/:id", validateSelectOptions, (req, res) => buildGetProduct().handle(req, res));
 
 // Users
 router.post("/signup", (req, res) => buildCreateUser().handle(req, res));
