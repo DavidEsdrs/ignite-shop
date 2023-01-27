17e51f055010267904d31d307f0d9d49d70d48f1
DavidEsdrs <carpina1983@gmail.com>
2023-01-27 17:25:05 -0300

    Add 'select' options to the find entities

--- src/services/products/GetProduct/GetProductService.ts
+++ src/services/products/GetProduct/GetProductService.ts
@@ -1,13 +1,23 @@
 import { IProductsRepository } from "../../../repositories/IProductsRepository";
-import { IGetProductDTO } from "./GetProductDTO";
+import { IGetProductDTO, ISelectOptions } from "./GetProductDTO";
 
 export class GetProductService {
+    default_options: ISelectOptions = {
+        title: true, 
+        description: true, 
+        price: true, 
+        image_path: true, 
+        created_at: true, 
+        updated_at: true 
+    };
+
     constructor(
         private productsRepository: IProductsRepository
     ) {}
 
-    async execute({ id }: IGetProductDTO) {
-        const product = await this.productsRepository.findById(id);
+    async execute({ id, select }: IGetProductDTO) {
+        const { title, description, price, image_path, created_at, updated_at } = select ?? this.default_options;
+        const product = await this.productsRepository.findById(id, { title, description, price, image_path, created_at, updated_at });
         if(!product) {
             throw new Error("The given product ID doesn't exist in the database!");
         }
