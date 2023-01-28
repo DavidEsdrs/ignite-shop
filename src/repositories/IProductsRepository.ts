d2901d74ebe51289bcd462daf1dd10dbd5e5d814
DavidEsdrs <carpina1983@gmail.com>
2023-01-28 05:23:35 -0300

    Add 'select' arg option to the 'getProducts' function

--- src/repositories/IProductsRepository.ts
+++ src/repositories/IProductsRepository.ts
@@ -5,6 +5,6 @@ export interface IProductsRepository {
     findByTitle(title: string): Promise<Product>;
     create(args: Partial<Product>): Product;
     save(product: Product): Promise<void>;
-    getProducts(args?: { take?: number }): Promise<Product[]>;
-    findById(id: string, take?: ISelectOptions): Promise<Product>;
+    getProducts(args?: { select?: ISelectOptions, take?: number }): Promise<Product[]>;
+    findById(id: string, select?: ISelectOptions): Promise<Product>;
 }
