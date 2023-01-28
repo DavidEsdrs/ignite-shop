1617ed43c04ff5804b6425f51cbb987fc9e11d6b
DavidEsdrs <carpina1983@gmail.com>
2023-01-28 20:16:41 -0300

    Add cors to the api

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import "express-async-errors";
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes";
+import cors from "cors";
 
 dotenv.config();
 
 const server = express();
-server.use(express.json());
 
+server.use(express.json());
+server.use(cors());
 server.use(router);
 
 const port = process.env.PORT || 5555;
