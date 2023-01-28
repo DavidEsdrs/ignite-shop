99da26c34a963568153fa64fe4d5dab23cc9bd74
DavidEsdrs <carpina1983@gmail.com>
2023-01-28 05:24:57 -0300

    Fix bug that retrieves wrong entity ID when feed functions with 'select' args

--- src/repositories/implementations/typeorm/ProductsRepository.ts
+++ src/repositories/implementations/typeorm/ProductsRepository.ts
@@ -1,6 +1,7 @@
 import AppDataSource from "../../../ormconfig";
 import { Product } from "../../../entities/Product";
 import { ISelectOptions } from "../../../services/products/GetProduct/GetProductDTO";
+import { IProductsRepository } from "../../IProductsRepository";
 
 export const productsRepository = AppDataSource.getRepository(Product).extend({
     async findByTitle(title: string) {
@@ -8,13 +9,25 @@ export const productsRepository = AppDataSource.getRepository(Product).extend({
         return product;
     },
 
-    async getProducts(args?: { take?: number; }) {
-        const products = await this.find({ take: args.take });
+    async getProducts(args?: { select?: ISelectOptions, take?: number; }) {
+        const products = await this.find({ 
+            take: args.take, 
+            select: args.select ? {
+                id: true,
+                ...args.select
+            } : undefined
+        });
         return products;
     },
 
     async findById(id: string, select?: ISelectOptions) {
-        const product = await this.findOne({ where: { id }, select });
+        const product = await this.findOne({ 
+            where: { id }, 
+            select: select ? {
+                id: true,
+                ...select
+            } : undefined
+        });
         return product;
     }
 });
