93fe8250f98e50b90ae04af3d201dc9c4a83bc19
DavidEsdrs <carpina1983@gmail.com>
2023-01-31 01:52:47 -0300

    Add 'removeProduct' DELETE method route

--- src/routes.ts
+++ src/routes.ts
@@ -5,12 +5,15 @@ import { buildAddProduct } from "./services/products/AddProduct/buildAddProductS
 import { buildGetProduct } from "./services/products/GetProduct/buildGetProduct";
 import { validateSelectOptions } from "./services/products/GetProduct/GetProduct.middleware";
 import { buildGetProducts } from "./services/products/GetProducts/buildGetProducts";
+import { buildRemoveProduct } from "./services/products/RemoveProduct/buildRemoveProduct";
 import { buildCreateUser } from "./services/users/CreateUser/buildCreateUser";
 
 const router = Router();
 
 // Products
 router.post("/products", validateProduct, (req, res) => buildAddProduct().handle(req, res));
+
+router.delete("/products/:id", (req, res) => buildRemoveProduct().handle(req, res));
 
 router.get("/products/all", validateQueryOptions, (req, res) => buildGetProducts().handle(req, res));
 
